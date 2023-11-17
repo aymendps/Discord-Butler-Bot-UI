967257b61a7f2651967d7ee28aff8cb6befef046
Aymen Hammami <hammami.aym@outlook.com>
2023-11-17 16:31:40 +0100

    initial commit

---
+++ Discord Butler Bot UI/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Discord_Butler_Bot_UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Bot_Click(object sender, RoutedEventArgs e)
+        {
+            StartBot.IsEnabled = false;
+            StartBot.Content = "Starting...";
+        }
+    }
+}
