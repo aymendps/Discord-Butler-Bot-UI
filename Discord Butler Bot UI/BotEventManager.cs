6d2fef0e3915c679171d05ba1cc47b7fad46a704
Aymen Hammami <hammami.aym@outlook.com>
2023-11-27 21:31:23 +0100

    added comments, added BotLog user control to be used to display bot logs

--- Discord Butler Bot UI/BotEventManager.cs
+++ Discord Butler Bot UI/BotEventManager.cs
@@ -4,10 +4,20 @@ namespace Discord_Butler_Bot_UI
 {
     enum BotEvent
     {
-        Started
+        Online,
+        JoinedChannel,
+        LeftChannel,
+        AddedSong,
+        PlayedSong,
+        SkippedSong,
     }
     internal class BotEventManager
     {
+        /// <summary>
+        /// Returns true if the given line represents a bot event of the given type.
+        /// </summary>
+        /// <param name="line">The string produced by the bot process</param>
+        /// <param name="botEvent">The type of bot event to check for</param>
         public static bool IsBotEvent(String? line, BotEvent botEvent)
         {
             return line != null && line.StartsWith("BotEvent." + botEvent.ToString());
---
+++ Discord Butler Bot UI/UserControls/BotLog.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Discord_Butler_Bot_UI.UserControls
+{
+    /// <summary>
+    /// Interaction logic for BotLog.xaml
+    /// </summary>
+    public partial class BotLog : UserControl
+    {
+        public static readonly DependencyProperty StatusColorProperty = 
+            DependencyProperty.Register("StatusColor", typeof(Brush), typeof(BotLog), new PropertyMetadata(Brushes.LightGray));
+
+        public Brush StatusColor
+        {
+            get { return (Brush)GetValue(StatusColorProperty); }
+            set { SetValue(StatusColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty LogDateTextProperty =
+            DependencyProperty.Register("LogDateText", typeof(string), typeof(BotLog), new PropertyMetadata(DateTime.Now.ToString("HH:mm:ss")));
+
+        public string LogDateText
+        {
+            get { return (string)GetValue(LogDateTextProperty); }
+            set { SetValue(LogDateTextProperty, value); }
+        }
+
+        public BotLog()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
